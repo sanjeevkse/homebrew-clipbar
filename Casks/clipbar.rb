8bcebf84747579d225f1757f0260bc9632eaf4e3
sanjeev shetty <sanjeevkse@gmail.com>
2026-01-01 15:01:59 +0530

    Update clipbar.rb

--- Casks/clipbar.rb
+++ Casks/clipbar.rb
@@ -2,16 +2,10 @@ cask "clipbar" do
   version "1.0.1"
   sha256 "7e8682ffc4690b5991e6b0d1ad1b8e2b0189095fb277212cd81daabdaf23417b"
 
-  url :github_release
+  url "https://sanjeevkse.github.io/ClipBar/downloads/ClipBar-#{version}-unsigned.zip"
   name "ClipBar"
   desc "Lightweight macOS menu bar clipboard manager"
   homepage "https://github.com/sanjeevkse/ClipBar"
 
   app "ClipBar.app"
-
-  caveats <<~EOS
-    ClipBar is not notarized.
-    macOS may show a security warning on first launch.
-    This is expected for open-source apps distributed outside the App Store.
-  EOS
 end
