7d062b8e40cd33d62618ca1f985909a6aa29dc8e
sanjeev shetty <sanjeevkse@gmail.com>
2026-01-01 14:01:54 +0530

    Update clipbar.rb

--- Casks/clipbar.rb
+++ Casks/clipbar.rb
@@ -2,7 +2,8 @@ cask "clipbar" do
   version "1.0.1"
   sha256 "7e8682ffc4690b5991e6b0d1ad1b8e2b0189095fb277212cd81daabdaf23417b"
 
-  url "https://github.com/sanjeevkse/ClipBar/releases/download/v#{version}/ClipBar-#{version}-unsigned.zip"
+  url "https://github.com/sanjeevkse/ClipBar/releases/download/v#{version}/ClipBar-#{version}-unsigned.zip",
+    verified: "github.com/sanjeevkse/ClipBar/"
   name "ClipBar"
   desc "Lightweight macOS menu bar clipboard manager"
   homepage "https://github.com/sanjeevkse/ClipBar"
